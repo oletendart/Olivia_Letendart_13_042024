fbab43db6e6920b9272ff818afdef5ad37d047e8
Olivia L <letendartolivia@gmail.com>
2024-04-22 12:03:59 +0200

    install react-router-dom + initialise router in the file app.jsx + create folder pages and create 2 pages, home and contact

--- argent-bank/src/App.jsx
+++ argent-bank/src/App.jsx
@@ -1,10 +1,18 @@
-import './App.scss'
+import './App.scss';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home/Home.jsx"
+import Contact from "./pages/Contact/Contact.jsx";
 
 function App() {
 
   return (
     <>
-      <p>Hello World</p>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
+          </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ argent-bank/src/pages/Contact/Contact.jsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return (
+        <div>
+            <p>Bienvenue sur contact</p>
+        </div>
+    )
+}
