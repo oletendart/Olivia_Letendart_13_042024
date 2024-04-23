526dbabf914efe20b56852cb5ea466bb42466017
Olivia L <letendartolivia@gmail.com>
2024-04-23 10:31:10 +0200

    create 2 components : footer and navbar + insert in the pages Connection and Home + insert link font awesome in the file index.html

---
+++ argent-bank/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import logo from "../../assets/argentBankLogo.png";
+
+export default function Navbar() {
+    return (
+        <nav className="main-nav">
+            <a className="main-nav-logo">
+                <img
+                    className="main-nav-logo-image"
+                    src={logo}
+                    alt="Argent Bank Logo"
+                />
+                <h1 className="sr-only">Argent Bank</h1>
+            </a>
+            <div>
+                <a className="main-nav-item"
+                   href="./sign-in.html">
+                    <i className="fa fa-user-circle"></i>
+                    Sign In
+                </a>
+            </div>
+        </nav>
+    )
+}
--- argent-bank/src/pages/Connection/Connection.jsx
+++ argent-bank/src/pages/Connection/Connection.jsx
@@ -1,7 +1,39 @@
+import Navbar from "../../components/Navbar/Navbar.jsx";
+import Footer from "../../components/Footer/Footer.jsx";
+
 export default function Connection() {
     return (
         <>
-            <p>Bienvenue sur connection</p>
-        </>
+            <Navbar />
+            <main className="main bg-dark">
+                <section className="sign-in-content">
+                    <i className="fa fa-user-circle sign-in-icon"></i>
+                    <h1>Sign In</h1>
+                    <form>
+                        <div className="input-wrapper">
+                            <label htmlFor="username">Username</label
+                            ><input type="text"
+                                    id="username"/>
+                        </div>
+                        <div className="input-wrapper">
+                            <label htmlFor="password">Password</label
+                            ><input type="password"
+                                    id="password"/>
+                        </div>
+                        <div className="input-remember">
+                            <input type="checkbox"
+                                   id="remember-me"/><label htmlFor="remember-me"
+                        >Remember me</label
+                        >
+                        </div>
+                        {/* PLACEHOLDER DUE TO STATIC SITE */}
+                        <a href="./user.html" className="sign-in-button">Sign In</a>
+                        {/* SHOULD BE THE BUTTON BELOW */}
+                        {/* <button class="sign-in-button">Sign In</button> */}
+                    </form>
+                </section>
+            </main>
+            <Footer />
+            </>
     )
 }
