919ee5fb8f3374b1bded77a978336724fd5d915e
Olivia L <letendartolivia@gmail.com>
2024-05-09 23:46:35 +0200

    insert Link to the component Navbar for logo argent bank return to the page home

--- argent-bank/src/components/Navbar/Navbar.jsx
+++ argent-bank/src/components/Navbar/Navbar.jsx
@@ -5,14 +5,14 @@ import {Link} from "react-router-dom";
 export default function Navbar() {
     return (
         <nav className="main-nav">
-            <a className="main-nav-logo">
+            <Link to="/" className="main-nav-logo">
                 <img
                     className="main-nav-logo-image"
                     src={logo}
                     alt="Argent Bank Logo"
                 />
                 <h1 className="sr-only">Argent Bank</h1>
-            </a>
+            </Link>
             <div>
                 <Link to="/login" className="main-nav-item">
                     <i className="fa fa-user-circle"></i>
