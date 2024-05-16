3c08a41af2e2903a107d14058ad37d882bb9f1f7
Olivia L <letendartolivia@gmail.com>
2024-05-17 00:04:34 +0200

    sémantique for page login

--- argent-bank/src/components/InputForm/InputForm.jsx
+++ argent-bank/src/components/InputForm/InputForm.jsx
@@ -3,9 +3,9 @@ import './InputForm.scss';
 export default function InputForm({htmlFor, text, type, id}) {
     return (
         <div className="input-wrapper">
-            <label htmlFor={htmlFor}>{text}</label
-            ><input type={type}
-                    id={id}/>
+            <label htmlFor={htmlFor}>{text}</label>
+            <input type={type}
+                   id={id}/>
         </div>
     )
 }
--- argent-bank/src/pages/Login/Login.jsx
+++ argent-bank/src/pages/Login/Login.jsx
@@ -7,20 +7,24 @@ import InputRemember from "../../components/InputRemember/InputRemember.jsx";
 export default function Login() {
     return (
         <>
-            <Navbar />
+            <header>
+                <Navbar />
+            </header>
             <main className="main bg-dark login-bg">
                 <section className="sign-in-content">
                     <i className="fa fa-user-circle sign-in-icon"></i>
                     <h1>Sign In</h1>
                     <form>
-                        <InputForm htmlFor={"username"} text={"Username"} type={"text"} id={"username"} />
-                        <InputForm htmlFor={"password"} text={"Password"} type={"password"} id={"password"}/>
+                        <InputForm htmlFor="username" text="Username" type="text" id="username" />
+                        <InputForm htmlFor="password" text="Password" type="password" id="password" />
                         <InputRemember />
-                        <button className="sign-in-button">Sign In</button>
+                        <button className="sign-in-button" type="submit">Sign In</button>
                     </form>
                 </section>
             </main>
-            <Footer />
-            </>
+            <footer>
+                <Footer />
+            </footer>
+        </>
     )
 }
